f2b2f8202a140ff9f2e01fd3d86c08f0056820fe
tetuyoko <tyokoyama53@gmail.com>
2016-10-26 17:49:44 +0900

    add softmax

--- section_3/softmax.py
+++ section_3/softmax.py
@@ -1,11 +1,13 @@
 import numpy as np
 
 def softmax(a):
-    exp_a = np.exp(a)
+    c = np.max(a)
+    exp_a = np.exp(a - c) #prevent overflow
     y = exp_a / np.sum(exp_a)
     return y
 
 a = np.array([0.3, 2.9, 4.0])
+a = np.array([1010, 1000, 900])
 y = softmax(a)
 
 print(y)
